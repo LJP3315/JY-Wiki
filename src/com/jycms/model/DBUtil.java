a25e212cdea9c9d3459c2ed2af1239ec0a04b5df
佳鹏 李 <3382353764@qq.com>
2025-11-26 20:26:04 +0800

    修改了数据库连接的工具类 删除了对 jdbc 外部包的无用查找

--- src/com/jycms/model/DBUtil.java
+++ src/com/jycms/model/DBUtil.java
@@ -5,19 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
+    // 数据库连接的常用变量
     private static final String URL = "jdbc:mysql://localhost:3306/jycms?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
-    private static final String PASS = "LiGaB3315"; // 请替换为您的数据库密码
+    private static final String PASS = "LiGaB3315";
 
-    static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.err.println("MySQL 驱动加载失败！");
-            e.printStackTrace();
-        }
-    }
-
+    // 为外部提供连接数据库的方法
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
